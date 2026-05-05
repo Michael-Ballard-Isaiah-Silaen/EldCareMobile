b7c94dca5f899a4eac7e8afd2a8a66d1940ce975
Michael-Ballard-Isaiah-Silaen <silaenmichael@gmail.com>
2026-05-05 22:13:08 +0700

    minor fix

--- EldCare-App/src/config/api.ts
+++ EldCare-App/src/config/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as SecureStore from 'expo-secure-store';
 
 const api = axios.create({
-  baseURL: 'https://eld-care-web-proto.vercel.app',
+  baseURL: 'https://eldcarebackend.vercel.app',
 });
 
 api.interceptors.request.use(async (config) => {
